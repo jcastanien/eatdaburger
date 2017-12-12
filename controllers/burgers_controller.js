901b026e402938eea1dbb314ed146feef481a9d1
jcastanien <jcastanien@gmail.com>
2017-12-11 23:43:30 -0500

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -28,13 +28,13 @@ router.post("/api/burgers", function(req, res) {
     });
 });
 
-router.put("/api/burgers:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
     burger.updateOne({
         devoured: req.body.devoured
     }, condition, function(result) {
-        if (results.changedRows == 0) {
+        if (result.changedRows == 0) {
             return res.status(404).end();
         } else {
             res.status(200).end();
