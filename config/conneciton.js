fb36a6a04013537ea6172cbbacd13b878d51820a
jcastanien <jcastanien@gmail.com>
2017-12-12 00:24:03 -0500

    update

--- config/conneciton.js
+++ config/conneciton.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(procss.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
     port: 3306,
